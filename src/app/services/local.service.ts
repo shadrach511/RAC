f1e6f5fd78cf5f26ca0a14e4cb1270a479f483b4
Jonas Paulsson <tarik.lustig@gmail.com>
2022-12-06 15:28:33 +0100

    created

--- src/app/services/local.service.ts
+++ src/app/services/local.service.ts
@@ -6,4 +6,19 @@ import { Injectable } from '@angular/core';
 export class LocalService {
 
   constructor() { }
+
+  public saveData(key: string, value: string) {
+    localStorage.setItem(key, value);
+  }
+
+  public getData(key: string) {
+    return localStorage.getItem(key)
+  }
+  public removeData(key: string) {
+    localStorage.removeItem(key);
+  }
+
+  public clearData() {
+    localStorage.clear();
+  }
 }
